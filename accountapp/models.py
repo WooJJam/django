88898582785fd391586d325d5a60456cf3fa132c
WooJJam <jaemin5548@naver.com>
2022-08-22 16:41:10 +0900

    Model, DB 연동

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False) # null 은 공백 true이면 공백이 있어도됨, false 이면 공백이 없어야함
+    
