a2018d7e7fb54507243f88f186a6a685672672e8
WooJJam <jaemin5548@naver.com>
2022-08-22 20:08:52 +0900

    POST 통신을 이용한 DB 데이터 저장

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,11 +9,11 @@ def hello_world(request):
 
     if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
+        temp = request.POST.get('text_input')
         new_hello_world = HelloWorld() # 새로운 객체가 new_hello_world에 저장됨
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        return render(request, 'accountapp/hello_world.html', context={'text_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET Method!!!'})
